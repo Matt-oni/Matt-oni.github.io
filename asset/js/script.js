c6c910bb169f46d9c17d8f8734d9675efc4ba283
Matteo <simonimatteo29@gmail.com>
2025-02-05 17:42:51 +0100

    Better hierarchy, new dark mode button

---
+++ asset/js/script.js
@@ -0,0 +1,26 @@
+﻿/* Dark Mode */
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+/* Typing Hero */
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "Hello friend, I'm Matteo.";
+    const typingElement = document.getElementById("typing-text");
+    let i = 0;
+
+    function typeWriter() {
+        if (i < text.length) {
+            typingElement.textContent += text.charAt(i); // Use textContent instead of innerHTML to avoid duplication
+            i++;
+            setTimeout(typeWriter, 100);
+        } else {
+            typingElement.innerHTML += '<span class="cursor">|</span>'; // Add the blinking cursor
+        }
+    }
+
+    // Ensure the text is empty before starting
+    typingElement.textContent = "";
+    typeWriter();
+});
